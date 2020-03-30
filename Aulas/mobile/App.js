59364112c5e5fd4bb2b0e1927d14d964e021ae29
gleyconxavier <gleyconxcarlos@gmail.com>
2020-03-29 23:29:00 -0400

    Updates

--- Aulas/mobile/App.js
+++ Aulas/mobile/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>Testando splash screen!</Text>
-    </View>
+    <Routes />
   );
 }
