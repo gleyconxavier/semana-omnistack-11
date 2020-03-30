cd1fe5d75c81eab97cbb2f5b2588e42e38f8a007
gleyconxavier <gleyconxcarlos@gmail.com>
2020-03-29 21:24:11 -0400

    Update | Frontend done.

--- Aulas/backend/src/controllers/IncidentController.js
+++ Aulas/backend/src/controllers/IncidentController.js
@@ -2,13 +2,29 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(req, res) {
-        const incidents = await connection('incidents').select('*');
+        const {
+            page = 1
+        } = req.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+
+        res.header('X-Total-Count', count['count(*)']);
 
         return res.json(incidents);
     },
 
     async create(req, res) {
-        const {title, description, value} = req.body;
+        const {
+            title,
+            description,
+            value
+        } = req.body;
         const ong_id = req.headers.authorization;
 
         const [id] = await connection('incidents').insert({
@@ -17,20 +33,26 @@ module.exports = {
             value,
             ong_id,
         });
-        return res.json({id});
+        return res.json({
+            id
+        });
     },
 
     async delete(req, res) {
-        const {id} = req.params;
+        const {
+            id
+        } = req.params;
         const ong_id = req.headers.authorization;
 
         const incident = await connection('incidents')
-        .where('id', id)
-        .select('ong_id')
-        .first();
+            .where('id', id)
+            .select('ong_id')
+            .first();
 
-        if(incident.ong_id !== ong_id) {
-            return res.status(401).json({error:'Operation not permitted.'});
+        if (incident.ong_id !== ong_id) {
+            return res.status(401).json({
+                error: 'Operation not permitted.'
+            });
         }
 
         await connection('incidents').where('id', id).delete();
